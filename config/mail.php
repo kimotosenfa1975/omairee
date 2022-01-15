5de1f2355bf5c1d8be7fdb4a05760c70e92ffef6
kimotosenfa1975 <superdev195128@hotmail.com>
2022-01-15 13:53:24 +0900

    re

--- config/mail.php
+++ config/mail.php
@@ -36,13 +36,12 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.lolipop.jp'),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('info@noisy-hita-1879.boy.jp'),
-            'password' => env('d-9mKDJWgwTUb21-'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'auth_mode' => null,
         ],
 
         'ses' => [
